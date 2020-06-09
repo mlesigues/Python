0d54d523949036f05988fc627a033f2a2333ccbd
mlesigues <marielle_lesigues029@yahoo.com>
2020-06-08 21:57:59 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,14 @@
+#introduction to python
+
+#print function = it prints anything you put on inside the parentheses. Ex:
+print ("Hello, World")
+print ("Hello, my name is Ate Marielle")
+
+#indentation is very important. It tells the compiler which block of codes belong together, if you misplace/skip an indentation, your code might cause an error or it might do what you tell it to do. Ex:
+def my_function():
+  print("this is the my_function") 
+
+my_function()
+
+#def my_function2():
+#print("this is my_function2")
