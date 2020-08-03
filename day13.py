a309dc0e9166c9d5765143c7553bcb117ba05e85
mlesigues <marielle_lesigues029@yahoo.com>
2020-08-02 23:16:55 -0700

    still not finish, this gives me error!

--- day13.py
+++ day13.py
@@ -3,42 +3,42 @@
 
 
 
-# Complete the solve function below.
-def solve(s):
-    #s[0] = s.capitalize()
-    #cap = s.capitalize()
-    # for i in len(range(s)):
-    #     if s[i] == " ":
-    #         s[i+1] = s.capitalize()
-    # for i in range(0, len(s)):
-    #     if [s+1] == " ":
-    #         cap = s.capitalize()
-    # strSplit = s.split(" ")
-    # for i in range(0, len(s)):
-    #     i[0] = s.capitalize()
-    #     if s[i] == strSplit:
-    #         if s[i+1] != strSplit:
-    #             s[i+1] = s.capitalize()
-    # return s
+# # Complete the solve function below.
+# def solve(s):
+#     #s[0] = s.capitalize()
+#     #cap = s.capitalize()
+#     # for i in len(range(s)):
+#     #     if s[i] == " ":
+#     #         s[i+1] = s.capitalize()
+#     # for i in range(0, len(s)):
+#     #     if [s+1] == " ":
+#     #         cap = s.capitalize()
+#     # strSplit = s.split(" ")
+#     # for i in range(0, len(s)):
+#     #     i[0] = s.capitalize()
+#     #     if s[i] == strSplit:
+#     #         if s[i+1] != strSplit:
+#     #             s[i+1] = s.capitalize()
+#     # return s
 
-    #newString = s
-    #new_string = newString.capitalize()
+#     #newString = s
+#     #new_string = newString.capitalize()
 
-    newString = s
-    new_string = ' '.join(map(str.capitalize, newString.split(' ')))
+#     newString = s
+#     new_string = ' '.join(map(str.capitalize, newString.split(' ')))
 
-    return new_string
+#     return new_string
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s = input()
+#     s = input()
 
-    result = solve(s)
+#     result = solve(s)
 
-    fptr.write(result + '\n')
+#     fptr.write(result + '\n')
 
-    fptr.close()
+#     fptr.close()
 
 #TASK: matrix addition.
 #input: user will input the two arrays => can be converted to matrices
@@ -62,15 +62,25 @@ else:
     #         result[i][j] = userLength1[i][j] + userLength2[i][j]
     # print(result)
     #iterate through rows
-    for i in range(userInput1):
-        firstArr = []
-        for j in range(userInput2):
-            firstArr.append(int(input()))
-        result.append(firstArr)
+    # for i in range(userInput1):
+    #     firstArr = []
+    #     for j in range(userInput2):
+    #         firstArr.append(int(input()))
+    #     result.append(firstArr)
 
-    #print 
-    for i in range(userInput1):
-        for j in range(userInput2):
-            print(result[i][j], end="")
-        print()
+    # #print 
+    # for i in range(userInput1):
+    #     for j in range(userInput2):
+    #         print(result[i][j], end="")
+    #     print()
+
+    firstArr = []
+    for i in len(range(userLength1)):
+        firstArr.append([i])
+    print (firstArr)
+
+    secondArr = []
+    for j in len(range(userLength2)):
+        secondArr.append([j])
+    print (secondArr)
 
