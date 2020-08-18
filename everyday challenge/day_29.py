c43fbaf6f3c25b36c45d115b9b7b466ba7c16b9d
mlesigues <marielle_lesigues029@yahoo.com>
2020-08-17 17:59:18 -0700

    Add files via upload

---
+++ everyday challenge/day_29.py
@@ -0,0 +1,15 @@
+#Task:
+#src: https://www.youtube.com/watch?v=IX4iFnEXrhM
+
+class Solution:
+    def distributeCandies(self, candies, num_people):
+        x = 0
+        candy_list = [0] * num_people
+        
+        while candies > 0:
+            candy_list[x % num_people] += min((x+1), candies)
+            x += 1
+            candies -= x 
+        
+        return candy_list
+            
