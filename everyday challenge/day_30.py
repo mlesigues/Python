be7b16703db4d75dbac293f3fbf5094aed455055
mlesigues <marielle_lesigues029@yahoo.com>
2020-08-18 22:29:49 -0700

    Add files via upload

---
+++ everyday challenge/day_30.py
@@ -0,0 +1,16 @@
+#Task: Sqrt(x) from Leetcode
+
+import math
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        ans = math.sqrt(x)
+        final = math.trunc(ans)
+        
+        return final
+        
+        
+        
+        
+
