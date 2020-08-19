25abcc9906c0f96d95b998ae18c53823fd6a5c5c
mlesigues <mlesigues@ucmerced.edu>
2020-08-18 22:38:03 -0700

    added new challenge

--- everyday challenge/day_30.py
+++ everyday challenge/day_30.py
@@ -9,7 +9,25 @@ class Solution:
         final = math.trunc(ans)
         
         return final
+    
+    
+#Task: Valid Perfect Square from Leetcode
+#src:https://www.geeksforgeeks.org/check-if-a-number-is-perfect-square-without-finding-square-root/
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+     #cannot use sqrt() function
+    
+        itr = 1
         
+        while(itr * itr <= num):
+            if ((num % itr == 0) and (num/itr == itr)):
+                return True
+            itr += 1
+            
+        return False
+
+
         
         
         
