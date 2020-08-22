01d0b082a1e5d02863ee3815400f2867bdc4aa5a
mlesigues <marielle_lesigues029@yahoo.com>
2020-08-21 20:33:25 -0700

    Add files via upload

--- everyday challenge/day_33.py
+++ everyday challenge/day_33.py
@@ -25,5 +25,36 @@ class Solution:
         #return listEven
         #return listOdd
         return listEven + listOdd
+
+
+#Task: Plus One from Leetcode
+#src: (on the else part)https://leetcode.com/problems/plus-one/discuss/24085/Simple-Python-solution-with-explanation-(Plus-One)
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
         
+        #if the list only contain 1 element
+        #if len(digits) == 1 and digits[0] != 9:
+        #    digits[0] = digits[0] + 1
+        #    return digits
+        
+        #esp. case, if element is number 9 => should return as [1,0]
+        if len(digits) == 1 and digits[0] == 9:
+             return [1, 0]
+            
+        #if more than one element, go to the end of the list and add 1
+        #else:
+        for i in range(len(digits)):
+            if i == (len(digits)-1) or digits[-1] != 9: #are we the end?
+                digits[-1] = digits[-1] + 1 #we're at the last element, then add 1
+                return digits
+            else:
+                #this handle if the nums is like this: [9, 9] or something like that 
+                digits[-1] = 0
+                digits[:-1] = self.plusOne(digits[:-1])
+                return digits
+            
+        #esp. case, if element is number 9 => should return as [1,0]
+        #if len(digits) == 1 and digits[0] == 9:
+        #     return [1, 0]
+            
         
