b317bea2a7d24b89b430660bcf5369b09ff7a8b7
mlesigues <mlesigues@ucmerced.edu>
2020-08-30 17:01:10 -0700

    Update day_41.py

--- everyday challenge/day_41.py
+++ everyday challenge/day_41.py
@@ -24,6 +24,44 @@ if __name__ == '__main__':
             avg = sum(score) / 3
             print ('{:0.2f}'. format(avg)) #2 decimal places
 
+            
+            
+#Task: Lists from Hackerrank
+if __name__ == '__main__':
+    N = int(input())
+
+    #NOTE: poorly worded, kind of confusing on how to start at first
+    #read the first word to match the commands, use split()
+
+    res = []
+
+    for elem in range(N):
+        #split to get the commands; make sure to only read the str arguments
+        com = input().strip().split(" ")
+        
+        if com[0] == "insert":
+            res.insert(int(com[1]), int(com[2]))
+
+        elif com[0] == "print":
+            print (res)
+
+        elif com[0] == "remove":
+            res.remove(int(com[1]))
+
+        elif com[0] == "append":
+            res.append (int (com[1]))
+
+        elif com[0] == "sort":
+            res.sort()
+
+        elif com[0] == "pop":
+            res.pop()
+
+        else:
+            #do reverse
+            res.reverse()
+        
+        
 
 
 
