ac14417295f89003c510b3226f4e77f3ce1e8026
mlesigues <mlesigues@ucmerced.edu>
2020-08-31 15:33:20 -0700

    Update day_42.py

--- everyday challenge/day_42.py
+++ everyday challenge/day_42.py
@@ -14,3 +14,40 @@ if __name__ == '__main__':
     i, c = input().split()
     s_new = mutate_string(s, int(i), c)
     print(s_new)
+
+    
+#Task: Nested Lists
+
+if __name__ == '__main__':
+    
+    record = []
+    lowest_name = []
+    scores = set()
+
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        record.append([name, score])
+        scores.add(score)
+
+    #sort the grades, get the 1st index (not 0)
+    lowest_score = sorted(scores)[1]
+
+    #print(scores)
+    #print(lowest_score)
+
+    for name, score in record:
+        if score == lowest_score:
+            lowest_name.append(name)
+
+    for name in sorted(lowest_name):
+        print(name, end="\n")
+
+
+
+
+
+
+        
+
+
