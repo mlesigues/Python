94dc229df635aa4382cd50c23f15a34d91a8bb82
mlesigues <marielle_lesigues029@yahoo.com>
2020-09-05 21:24:13 -0700

    Add files via upload

---
+++ everyday challenge/day_45.py
@@ -0,0 +1,28 @@
+#Task: Symmetric Difference from Hackerrank
+#Note: it works(passed all tests), but the newline printing could be better!
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+m = input().split(' ')
+#arrElems_M = [int(i) for i in input().split(' ')]
+arrElems_M = set([int(i) for i in input().split(' ')])
+n = input().split(' ')
+arrElems_N = set([int(i) for i in input().split(' ')])
+
+#goal: symmetric difference in asc order, one per line
+
+#symmetric difference: indicates those values that exist in either M or N, but not both
+partial = arrElems_M.union(arrElems_N)
+res_m = arrElems_M.difference(arrElems_N) #Values which exist in m but not in n
+res_n = arrElems_N.difference(arrElems_M) #Values which exist in n but not in m
+
+#convert sets to lists
+res_m = list(res_m)
+res_n = list(res_n)
+res = sorted(res_m + res_n) #asc sort
+for elem in res:
+    print(elem)
+print('\n')
+
+
+
+
+
