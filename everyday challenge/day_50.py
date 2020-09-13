404b3a9d87a614168c1d1030a8dcb903db5fa977
mlesigues <mlesigues@ucmerced.edu>
2020-09-12 18:28:22 -0700

    Update day_50.py

--- everyday challenge/day_50.py
+++ everyday challenge/day_50.py
@@ -1,4 +1,4 @@
-#Task: Binomial Distribution I
+#Task: Day 4: Binomial Distribution I
 #factorial
 def fact(n):
     if n == 0 or n == 1:
@@ -30,3 +30,46 @@ for x in range(3, 7): #3-7 because it'll loop 4 times
     res += binom(x,n,p)
 res = round(res,3)
 print(res)
+
+
+
+#Task: Day 4: Binomial Distribution II
+#factorial
+def fact(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n-1)
+
+#combinations: n! / x! (n-x!)
+def comb(n, x):
+    return fact(n)/(fact(x)*fact(n-x))
+
+
+#binomial: combinations * p^x * q^(n-x); q = 1-p therefore (1-p)**(n-x)
+#note: exponention is '**'
+def binom(x,n,p):
+    return comb(n, x) * (p**x) * (1-p)**(n-x)
+
+values = list(map(int,input().split(' ')))
+n = values[1]
+
+#p is 12%, so 12/100
+p = 12/100
+
+
+#a batch of 10 pistons will contain no more than 2 rejects
+no_more_two = 0
+for x in range(n):
+    if x < 3:
+        no_more_two += binom(x, n, p)
+no_more_two = round(no_more_two, 3)
+print(no_more_two)
+
+#a batch of 10 pistons will contain at least 2 rejects
+at_least_two = 0
+for x in range(n):
+    if x >= 2:
+        at_least_two += binom(x, n, p)
+at_least_two = round(at_least_two, 3)
+print(at_least_two)
