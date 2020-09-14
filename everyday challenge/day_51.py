0a78a99d681ac557824722115a0744c3b3cde745
mlesigues <mlesigues@ucmerced.edu>
2020-09-13 19:12:34 -0700

    Update day_51.py

--- everyday challenge/day_51.py
+++ everyday challenge/day_51.py
@@ -18,3 +18,31 @@ class Solution:
         
         return result
                 
+
+        
+        
+#Task: Day 5: Poisson Distribution from Hackerrank
+import math
+x_mean = float(input())
+want_probs = int(input())
+
+#factorial
+def fact(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n-1)
+
+#poisson dist: P(k, lambda) = (lambda)^(k) * e^(-lambda) / k!
+def poisson(k, lamb):
+    res = (lamb**k) * (math.e**-lamb) / fact(k)
+    return res
+
+result = poisson(want_probs, x_mean)
+result = round(result, 3)
+print(result)
+
+
+
+
+#Task: Day 5:
