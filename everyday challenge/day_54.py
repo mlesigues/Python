bf9d6b4baa8c95aa0b497137f843da06cbcc82e8
mlesigues <mlesigues@ucmerced.edu>
2020-09-16 21:37:25 -0700

    Update day_54.py

--- everyday challenge/day_54.py
+++ everyday challenge/day_54.py
@@ -53,3 +53,35 @@ class Solution:
             max_ = max(max_, self.findMax_xor(elems))
         
         return max_
+
+    
+    
+    
+#Task: Day 7: Pearson Correlation Coefficient I from Hackerrank Statistics
+import math
+
+n = int(input())
+data_set_X = list(map(float, input().strip().split())) #ensures we get 1 elem at a time
+data_set_Y = list(map(float, input().strip().split()))
+
+#calculate(mu * x) and (mu * y) these are both means of X and Y, respectively
+#to get mean, sum all the trials and divide by how many trial times
+mu_x = sum(data_set_X)/n
+mu_y = sum(data_set_Y)/n
+
+#calculate (sigma * X) and (sigma * Y) these are both standard deviations of X and Y
+stdDev_x = math.sqrt(sum([(elem - mu_x)**2 for elem in data_set_X]) / n)
+stdDev_y = math.sqrt(sum([(elem - mu_y)**2 for elem in data_set_Y]) / n)
+
+#compute covariance; cov(X,Y)
+cov_x_y = sum([(data_set_X[i] - mu_x) * (data_set_Y[i] - mu_y) for i in range (n)])
+
+#compute Pearson Correlation Coefficient
+pearson_correlation_coeff = cov_x_y/(stdDev_x * stdDev_y*n)
+
+print(round(pearson_correlation_coeff, 3))
+
+
+
+
+#Task: Day 7:
