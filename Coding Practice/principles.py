a85ae737a9dc05e51260e0ee7abb59f5842c3403
mlesigues <marielle_lesigues029@yahoo.com>
2021-09-30 10:10:31 -0700

    Create principles.py

---
+++ Coding Practice/principles.py
@@ -0,0 +1,16 @@
+#link: https://pythonprinciples.com/challenges/Capital-indexes/
+
+#Python Challenge: Write a function named capital_indexes. The function takes a single parameter, which is a string. 
+#Your function should return a list of all the indexes in the string that have capital letters.
+
+def capital_indexes(userInput):
+    res = []
+    for ind, elem in enumerate(userInput):
+        #print(ind, "==" ,elem)
+        if (elem.isupper() == True):
+            res.append(ind)
+    return res
+        
+#main   
+userInput = "HeLlO"
+capital_indexes(userInput)
