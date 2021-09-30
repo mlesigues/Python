ab260d502e142dcdb4765e6fec463eb3499ea295
mlesigues <marielle_lesigues029@yahoo.com>
2021-09-30 10:11:11 -0700

    Update principles.py

--- Coding Practice/principles.py
+++ Coding Practice/principles.py
@@ -1,4 +1,4 @@
-#link: https://pythonprinciples.com/challenges/Capital-indexes/
+#link: https://pythonprinciples.com/challenges/
 
 #Python Challenge: Write a function named capital_indexes. The function takes a single parameter, which is a string. 
 #Your function should return a list of all the indexes in the string that have capital letters.
