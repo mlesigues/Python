2730f33f03a8d4a3d5ed1949401d0e9e8096918a
mlesigues <marielle_lesigues029@yahoo.com>
2021-10-06 11:03:05 -0700

    Update principles.py

--- Coding Practice/principles.py
+++ Coding Practice/principles.py
@@ -45,3 +45,22 @@ mid(userInput)
 
 
 
+
+#3. 
+
+
+
+
+#4. Define a function, random_number, that takes no parameters. The function must generate a random integer between 1 and 100, both inclusive, and return it.
+#Calling the function multiple times should (usually) return different numbers.
+
+import random
+
+def random_number():
+  res = random.randint(1, 100) #inclusive
+  print(res)
+  return res
+
+random_number()
+
+
